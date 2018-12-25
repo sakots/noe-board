5eea0be76ba612969a0d47f4c53d9eb70d8fcdcd
sakots <sakots8000@gmail.com>
2018-12-25 20:05:21 +0900

    v0.3.1

--- noe/paint.php
+++ noe/paint.php
@@ -1,13 +1,13 @@
 <?php
 //--------------------------------------------------
-//　おえかきけいじばん「noe-board」v0.3.0
+//　おえかきけいじばん「noe-board」v0.3.1
 //　by sakots https://sakots.red/
 //--------------------------------------------------
 
 //Skinny 0.4.1
 include_once( "Skinny.php" );
 $out = array();
-$out["ver"] = "v0.3.0";
+$out["ver"] = "v0.3.1";
 
 //設定の読み込み
 require("config.php");
