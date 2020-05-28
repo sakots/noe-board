a7e7c0e269cb0ef303be207c12aac6162ff2497e
sakots <sakots8000@gmail.com>
2020-05-28 14:16:37 +0900

    v0.13.3

--- noe-board/index.php
+++ noe-board/index.php
@@ -9,7 +9,7 @@ require_once(__DIR__.'/libs/Smarty.class.php');
 $smarty = new Smarty();
 
 //スクリプトのバージョン
-$smarty->assign('ver','v0.13.2');
+$smarty->assign('ver','v0.13.3');
 
 //設定の読み込み
 require(__DIR__."/config.php");
