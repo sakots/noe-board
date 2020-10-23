cbf82d56050f391042839f54dc1edd51bb99788f
sakots <sakots8000@gmail.com>
2020-10-24 03:07:21 +0900

    v0.31.1

--- noe-board/index.php
+++ noe-board/index.php
@@ -5,7 +5,7 @@
 //--------------------------------------------------
 
 //スクリプトのバージョン
-define('NOE_VER','v0.31.0'); //lot.200716.0
+define('NOE_VER','v0.31.1'); //lot.201024.0
 
 //smarty-3.1.34
 require_once(__DIR__.'/libs/Smarty.class.php');
